130005dd6b5124fab4f417fd06a9375af205f4c7
Enoque Leal <enoquefelipe@gmail.com>
2022-01-18 18:06:04 -0300

    atualizando gitignore

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = options => {
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+            publicPath: "http://localhost:3000/",
+            uniqueName: "sample"
+        },
+        module: {
+            rules: [
+                {
+                    test: /.js$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                cacheDirectory: true,
+                                presets: ['@babel/react', '@babel/env']
+                            }
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.(svg|png|jpg|gif)$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 8192,
+                            },
+                        },
+                    ],
+                },
+
+            ],
+        },
+        plugins: [
+            new ModuleFederationPlugin({
+
+                name: "sample",
+                library: { type: "var", name: "sample" },
+                filename: "remoteEntry.js",
+                exposes: {
+                    './web-components': './src/App.js',
+                },
+
+                shared: ["react", "react-dom"]
+            })
+        ],
+        devServer: {
+            contentBase: "public",
+            port: 3000,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            }
+        }
+    }
+}
